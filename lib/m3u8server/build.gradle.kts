070cc776eb5cf6f67bb344d3a704060b3b3e46ed
H1steria <samuelmoramarin@gmail.com>
2026-08-17 17:09:19 -0500

    configure_CI

--- lib/m3u8server/build.gradle.kts
+++ lib/m3u8server/build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 dependencies {
-    implementation("com.github.NanoHttpd.nanohttpd:nanohttpd:-SNAPSHOT")
+    implementation(files("nanohttpd-2.3.2-efb2ebf85a2b06f7c508aba9eaad5377e3a01e81.jar"))
 }
